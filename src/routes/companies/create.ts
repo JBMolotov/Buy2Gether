d5f73fdc7244f614c158a646f1a2563eddf9cdf1
Rafael <rafajdantas@gmail.com>
2023-10-26 20:12:36 -0300

    update companies/create.ts

--- src/routes/companies/create.ts
+++ src/routes/companies/create.ts
@@ -9,12 +9,29 @@ createCompanyRouter.post("/", async (req, res) => {
   const manager = AppDataSource.createEntityManager();
   const companyData = req.body;
 
-  if(companyIsValid(companyData)){
-    const company = manager.create(Company, companyData);
-    
-    await manager.save(Company, company);
-    res.send(company);
+  // busca se ja tem empresa cadastrado com esse cpf/cnpj 
+  // se tiver, retorna empresa
+  // se nao tiver, retorna nulo
+  const c = await AppDataSource.getRepository(Company).findOneBy({
+    cpfCnpj: companyData.cpfCnpj
+  });
+
+  if(c === null){ // nao tem empresa cadastrada com esse cpf/cnpj
+    // verifica se dados sao validos
+    if(companyIsValid(companyData)){
+      const company = manager.create(Company, companyData);
+      await manager.save(Company, company);
+      res.send(company);
+    }
+    else{
+      console.log("Invalid data");
+      res.send("Invalid data");
+    };
   }
+  else{ // ja tem empresa cadastrado com esse cpf
+    console.log("CPF/CNPJ already registred");
+    res.send("CPF/CNPJ already registred");
+  };
 
   //res.send("Erro")
 });
