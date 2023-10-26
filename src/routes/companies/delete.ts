46e68075536250fe4670675d1bc257f6533cbb43
Rafael <rafajdantas@gmail.com>
2023-10-26 20:18:47 -0300

    update companies/delete.ts

--- src/routes/companies/delete.ts
+++ src/routes/companies/delete.ts
@@ -1,20 +1,17 @@
 import { Router } from "express";
-import { Client } from "../../entity/Client";
 import { AppDataSource } from "../../data-source";
-import { clientIsValid } from "../../utils/validation/validation";
+import { Company } from "../../entity/Company";
 
 export const deleteCompanyRouter = Router();
 
-deleteCompanyRouter.put("/", async (req, res) => {
-  const manager = AppDataSource.createEntityManager();
-  const clientData = req.body;
-
-  // if(clientIsValid(clientData)){
-  //   const client = manager.create(Client, clientData);
-    
-  //   await manager.save(Client, client);
-  //   res.send(client);
-  // }
-
-  // //res.send("Erro")
+deleteCompanyRouter.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  await AppDataSource
+    .createQueryBuilder()
+    .delete()
+    .from(Company)
+    .where("id = :id", { id: +id })
+    .execute();
+  console.log("Company deleted");
+  res.send("Company deleted");
 });
