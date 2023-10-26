c75af94897bfa7f95d8dba177ef673d718463177
Rafael <rafajdantas@gmail.com>
2023-10-26 03:28:57 -0300

    update clients/update.ts

--- src/routes/clients/update.ts
+++ src/routes/clients/update.ts
@@ -6,22 +6,36 @@ import { clientIsValid } from "../../utils/validation/validation";
 export const updateClientRouter = Router();
 
 updateClientRouter.put("/", async (req, res) => {
+
   const clientData = req.body;
+  //console.log(clientData);
 
-  if(clientIsValid(clientData)){
-    // const manager = AppDataSource.createEntityManager();
-    const repository = AppDataSource.getRepository(Client)
-    const client = await repository.findOneBy({
-      cpf: clientData.cpf
-    })
-    console.log(client)
-    console.log(clientData)
+  const repository = await AppDataSource.getRepository(Client)
+  const client = await repository.findOneBy({
+    cpf: clientData.cpf
+  });
+  //console.log(client)
 
-    // AppDataSource.createQueryBuilder().update(client)
-    //   .set({name: clientData.name, email: clientData.email, password: clientData.password, adress: clientData.adress, phoneNumber: clientData.phoneNumber,})
-    //   .where("id  :id").execute;
-
-    //await manager.save(Client, client);
-    res.send(clientData);
+  if(client != null){
+    if(clientIsValid(clientData)){
+      await repository.save({
+        id: client.id,
+        cpf: clientData.cpf,
+        name: clientData.name,
+        email: clientData.email,
+        password: clientData.password,
+        address: clientData.address,
+        phoneNumber: clientData.phoneNumber
+      });
+      res.send(clientData);
+    }
+    else{
+      console.log("Invalid data");
+      res.send("Invalid data");
+    }
+  }
+  else{
+    console.log("No register found");
+    res.send("No register found");
   }
 });
