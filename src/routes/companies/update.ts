df815ca313a3891de4b343a603e69a6636b33af0
Rafael <rafajdantas@gmail.com>
2023-10-26 20:17:30 -0300

    update companies/update.ts

--- src/routes/companies/update.ts
+++ src/routes/companies/update.ts
@@ -1,20 +1,40 @@
 import { Router } from "express";
-import { Client } from "../../entity/Client";
 import { AppDataSource } from "../../data-source";
-import { clientIsValid } from "../../utils/validation/validation";
+import { Company } from "../../entity/Company";
+import { companyIsValid } from "../../utils/validation/validation";
 
 export const updateCompanyRouter = Router();
 
 updateCompanyRouter.put("/", async (req, res) => {
-  const manager = AppDataSource.createEntityManager();
-  const clientData = req.body;
+  const companyData = req.body;
+  //console.log(companyData);
 
-  // if(clientIsValid(clientData)){
-  //   const client = manager.create(Client, clientData);
-    
-  //   await manager.save(Client, client);
-  //   res.send(client);
-  // }
+  const repository = await AppDataSource.getRepository(Company)
+  const company = await repository.findOneBy({
+    cpfCnpj: companyData.cpfCnpj
+  });
+  //console.log(company)
 
-  // //res.send("Erro")
+  if(company != null){
+    if(companyIsValid(companyData)){
+      await repository.save({
+        id: company.id,
+        cpfCnpj: companyData.cpfCnpj,
+        name: companyData.name,
+        email: companyData.email,
+        password: companyData.password,
+        address: companyData.address,
+        fieldOfActivity: companyData.fieldOfActivity
+      });
+      res.send(companyData);
+    }
+    else{
+      console.log("Invalid data");
+      res.send("Invalid data");
+    }
+  }
+  else{
+    console.log("No register found");
+    res.send("No register found");
+  }
 });
