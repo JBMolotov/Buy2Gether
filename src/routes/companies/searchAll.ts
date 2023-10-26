1db48bb1f76aa58eafc6ed62e43b798922bfb4f8
Rafael <rafajdantas@gmail.com>
2023-10-26 20:16:19 -0300

    update companies/searchAll.ts

--- src/routes/companies/searchAll.ts
+++ src/routes/companies/searchAll.ts
@@ -1,24 +1,12 @@
 import { Router } from "express";
-import { Client } from "../../entity/Client";
+import { Company } from "../../entity/Company";
 import { AppDataSource } from "../../data-source";
-import { clientIsValid } from "../../utils/validation/validation";
 
 export const searchAllCompanyRouter = Router();
 
 searchAllCompanyRouter.get("/", async (req, res) => {
   const manager = AppDataSource.createEntityManager();
-  const cpf = req.params;
+  const company = await manager.find(Company);
 
-  // const client = await Client.findOneBy({
-  //   cpf: ,
-  // })
-
-  // if(clientIsValid(clientData)){
-  //   const client = manager.create(Client, clientData);
-    
-  //   await manager.save(Client, client);
-  //   res.send(client);
-  // }
-
-  // //res.send("Erro")
+  res.send(company);
 });
