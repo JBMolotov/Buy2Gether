86cd72df1095361b728f476799d3569e9f50de2a
Rafael <rafajdantas@gmail.com>
2023-10-30 17:00:37 -0300

    update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
+
 import { createClientRouter } from "./clients/create";
 import { updateClientRouter } from "./clients/update";
 import { searchClientRouter } from "./clients/search";
 import { searchAllClientRouter } from "./clients/searchAll";
 import { deleteClientRouter } from "./clients/delete";
+
 import { createCompanyRouter } from "./companies/create";
 import { updateCompanyRouter } from "./companies/update";
 import { searchCompanyRouter } from "./companies/search";
 import { searchAllCompanyRouter } from "./companies/searchAll";
 import { deleteCompanyRouter } from "./companies/delete";
+
+import { companiesToApproveRouter } from "./companies/companiesToApprove";
+import { approvedCompaniesRouter } from "./companies/approvedCompanies";
 
 export const router = Router();
 
@@ -25,3 +30,5 @@ router.use("/companies/update", updateCompanyRouter);
 router.use("/companies/search", searchCompanyRouter);
 router.use("/companies/searchAll", searchAllCompanyRouter);
 router.use("/companies/delete", deleteCompanyRouter);
+router.use("/companies/companiesToApprove", companiesToApproveRouter);
+router.use("/companies/approvedCompanies", approvedCompaniesRouter);
