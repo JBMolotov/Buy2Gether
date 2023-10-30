91b41477006e8c7cbf63cd2617082d104fdeda7f
Rafael <rafajdantas@gmail.com>
2023-10-30 16:59:18 -0300

    update indext.ts and router.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,11 @@ require("./clients/search");
 require("./clients/searchAll");
 require("./clients/delete");
 
-//company
+// company
 require("./companies/create");
 require("./companies/update");
 require("./companies/search");
 require("./companies/searchAll");
 require("./companies/delete");
+require("./companies/companiesToApprove");
+require("./companies/approvedCompanies");
