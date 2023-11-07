df3c6c824a0547983fc75cc734d0f5b35b3e240b
Rafael <rafajdantas@gmail.com>
2023-11-06 21:36:38 -0300

    update routes/offers/delete

--- src/routes/offers/delete.ts
+++ src/routes/offers/delete.ts
@@ -1,19 +1,23 @@
 import { Router } from "express";
 import { AppDataSource } from "../../data-source";
 import { Offer } from "../../entity/Offer";
+import { Client } from "../../entity/Client";
 
 export const deleteOfferRouter = Router();
 
-// TODO: resolver o problema do delete
-
-// delete por id
+// delete by id
 deleteOfferRouter.delete("/:id", async (req, res) => {
-    
+
+    // deleta os registros da oferta na tabela client_offer_permission
+    await AppDataSource.query('delete from client_offer_permission where offer_id = ' + +req.params.id);
+
+    // atualiza os dados da oferta
     await AppDataSource.getRepository(Offer).save({
        id: +req.params.id,
-       deletedAt: new Date()
+       deletedAt: new Date(),
+       isDeleted: true
     });
-    
+
     console.log("Offer deleted");
     res.send("Offer deleted");
 });
