8e0bd11f79695e549f358a770620e51fba20c554
Rafael <rafajdantas@gmail.com>
2023-11-10 12:16:36 -0300

    update offers/create

--- src/routes/offers/create.ts
+++ src/routes/offers/create.ts
@@ -5,12 +5,13 @@ import { Offer } from "../../entity/Offer";
 
 export const createOfferRouter = Router();
 
-createOfferRouter.post("/", async (req, res) => {
+createOfferRouter.post("/:companyId", async (req, res) => {
     const manager = AppDataSource.createEntityManager();
     const offerData = req.body;
 
     if(offerIsValid(offerData)){
         const newOffer = manager.create(Offer, offerData);
+        newOffer.companyId = +req.params.companyId;
         await manager.save(Offer, newOffer);
         console.log("Registered offer");
         console.log(newOffer);
