f9292785a4335fdc12dfd7aa9bce8ee1bd74787b
Rafael <rafajdantas@gmail.com>
2023-11-10 12:15:48 -0300

    update feedbacks/create

--- src/routes/feedbacks/create.ts
+++ src/routes/feedbacks/create.ts
@@ -5,13 +5,14 @@ import { Feedback } from "../../entity/Feedback";
 
 export const createFeedbackRouter = Router();
 
-createFeedbackRouter.post("/:clientId/:offerId", async (req, res) => {
+createFeedbackRouter.post("/:companyId/:clientId/:offerId", async (req, res) => {
 
     const manager = AppDataSource.createEntityManager();
     const feedbackData = req.body;
 
     if(feedbackData != null){
         const newFeedback = manager.create(Feedback, feedbackData);
+        newFeedback.companyId = +req.params.companyId;
         newFeedback.offerId = +req.params.offerId;
         newFeedback.clientId = +req.params.clientId;
         await manager.save(Feedback, newFeedback);
