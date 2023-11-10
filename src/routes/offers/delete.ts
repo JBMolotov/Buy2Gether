f9727a2d8cb5411664eae441e05c362baf36b67b
Rafael <rafajdantas@gmail.com>
2023-11-10 12:26:52 -0300

    update offers/delete

--- src/routes/offers/delete.ts
+++ src/routes/offers/delete.ts
@@ -7,17 +7,29 @@ export const deleteOfferRouter = Router();
 // delete by id
 deleteOfferRouter.delete("/:id", async (req, res) => {
 
-    // deleta os registros da oferta na tabela client_offer_permission
-    // remove os cliente da oferta
-    await AppDataSource.query('delete from client_offer_permission where offer_id = ' + +req.params.id);
-
-    // atualiza os dados da oferta
-    await AppDataSource.getRepository(Offer).save({
-       id: +req.params.id,
-       deletedAt: new Date(),
-       isDeleted: true
+    const registeredOffer = await AppDataSource.getRepository(Offer).findOneBy({
+        id: +req.params.id
     });
 
-    console.log("Offer deleted");
-    res.send("Offer deleted");
+    //console.log(registeredOffer);
+
+    if(registeredOffer != null){
+        // deleta os registros da oferta na tabela client_offer_permission
+        // remove os cliente da oferta
+        await AppDataSource.query('delete from client_offer_permission where offer_id = ' + +req.params.id);
+
+        // atualiza os dados da oferta
+        await AppDataSource.getRepository(Offer).save({
+            id: +req.params.id,
+            deletedAt: new Date(),
+            isDeleted: true
+        });
+
+        console.log("Offer deleted");
+        res.send("Offer deleted");
+    }
+    else{
+        console.log("No offer found with given id");
+        res.send("No offer found with given id");
+    }
 });
