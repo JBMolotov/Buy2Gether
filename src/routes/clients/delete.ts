0f15301afc3fd5118a55a1bf1c39d3023dd53fc6
Rafael <rafajdantas@gmail.com>
2023-11-10 11:45:12 -0300

    update clients/delete to delete client from feedback

--- src/routes/clients/delete.ts
+++ src/routes/clients/delete.ts
@@ -1,12 +1,24 @@
 import { Router } from "express";
 import { Client } from "../../entity/Client";
 import { AppDataSource } from "../../data-source";
+import { Feedback } from "../../entity/Feedback";
 
 export const deleteClientRouter = Router();
 
 // delete por id
 deleteClientRouter.delete("/:id", async (req, res) => {
   const id = req.params.id;
+
+  // ao inves de deletar o feedback quando deletar o cliente
+  // o feedback ainda vai existir, mas o id do cliente fica -1
+  // quer dizer que a conta do cliente foi deletada
+  await AppDataSource
+    .createQueryBuilder()
+    .update(Feedback)
+    .set({ clientId: -1 })
+    .where({ clientId: +id })
+    .execute();
+
   await AppDataSource
     .createQueryBuilder()
     .delete()
