027f43bd2b9baa84578b7f831736c8508723fb98
Johnny <johnnyy.br@gmail.com>
2023-12-03 21:23:25 -0300

    Coisas pra funcionar

--- src/utils/validation/validation.ts
+++ src/utils/validation/validation.ts
@@ -2,94 +2,117 @@ import { Client } from "../../entity/Client";
 import { Company } from "../../entity/Company";
 import { Offer } from "../../entity/Offer";
 
-export function clientIsValid(client: Client): boolean{
-
-  if(!cpfCnpjIsValid(client.cpf)){
+export function clientIsValid(client: Client): boolean {
+  if (!isCPFValid(client.cpf)) {
+    console.log("cpfCnpjIsValid");
     return false;
   }
 
-  if(!emailIsValid(client.email)){
-    return false
-  }
-
-  if(!passwordIsValid(client.password)){
+  if (!emailIsValid(client.email)) {
+    console.log("emailIsValid");
     return false;
   }
 
-  if(!phoneNumberIsValid(client.phoneNumber)){
+  if (!passwordIsValid(client.password)) {
+    console.log("passwordIsValid");
+    return false;
+  }
+
+  if (!phoneNumberIsValid(client.phoneNumber)) {
+    console.log("phoneNumberIsValid");
     return false;
   }
 
   console.log("clientIsValid");
-  
+
   return true;
-};
+}
 
-export function companyIsValid(company: Company): boolean{
-
-  if(!cpfCnpjIsValid(company.cpfCnpj)){
+export function companyIsValid(company: Company): boolean {
+  if (!cpfCnpjIsValid(company.cpfCnpj)) {
+    console.log("cpfCnpjIsValid");
     return false;
   }
 
-  if(!emailIsValid(company.email)){
-    return false
-  }
-
-  if(!passwordIsValid(company.password)){
+  if (!emailIsValid(company.email)) {
+    console.log("emailIsValid");
     return false;
   }
 
-  if(!fieldOfActivityIsValid(company.fieldOfActivity)){
+  if (!passwordIsValid(company.password)) {
+    console.log("passwordIsValid");
+    return false;
+  }
+
+  if (!fieldOfActivityIsValid(company.fieldOfActivity)) {
+    console.log("fieldOfActivityIsValid");
     return false;
   }
 
   console.log("companyIsValid");
 
   return true;
-};
+}
 
-export function offerIsValid(offer: Offer): boolean{
-
-  if(offer.price < 0 || offer.minimalForFreeDelivery < 0 || offer.minimalForConsolidation < 0){
+export function offerIsValid(offer: Offer): boolean {
+  if (
+    offer.price < 0 ||
+    offer.minimalForFreeDelivery < 0 ||
+    offer.minimalForConsolidation < 0
+  ) {
     return false;
   }
 
   return true;
-};
+}
 
-function cpfCnpjIsValid(cpfCnpj: string): boolean{
+function cpfCnpjIsValid(cpfCnpj: string): boolean {
   // https://medium.com/shopify-hub/como-validar-cpf-e-cnpj-usando-expressões-regulares-regex-com-javascript-60779229455d
   // https://pt.stackoverflow.com/questions/11045/expressão-regular-para-validar-um-campo-que-aceita-cpf-ou-cnpj
-  const regex =  /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/;
-  return regex.test(cpfCnpj)
-};
+  cpfCnpj = cpfCnpj.replace(/[^\d]+/g, "");
+  const cnpjRegex = /^\d{15}$/;
+  return cnpjRegex.test(cpfCnpj);
+}
 
-function emailIsValid(email: string): boolean{
+export function isCPFValid(cpf: string): boolean {
+  // remove a máscara se tiver 12 remove o ultimo
+  cpf = cpf.replace(/[^\d]+/g, "");
+  if (cpf.length > 11) {
+    cpf = cpf.substring(0, 11);
+  }
+  console.log(cpf);
+  const cpfRegex = /^\d{11}$/;
+  return cpfRegex.test(cpf);
+}
+
+function emailIsValid(email: string): boolean {
   // https://pt.stackoverflow.com/questions/1386/expressão-regular-para-validação-de-e-mail
   const regex = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
-  return regex.test(email)
-};
+  return regex.test(email);
+}
 
-function passwordIsValid(password: string): boolean{
+function passwordIsValid(password: string): boolean {
   // https://pt.stackoverflow.com/questions/71709/verificar-input-senha
   // (?=(?:.*?[A-Z]){x}) - Mínimo x letras maiúsculas
   // (?=(?:.*?[0-9]){y}) - Mínimo y números
   // (?=(?:.*?[!@#$%*()_+^&}{:;?.]){z})(?!.*\s)[0-9a-zA-Z!@#;$%*(){}_+^&] - Mínimo z caractere especial
-  const regex = /^(?=(?:.*?[A-Z]){1})(?=(?:.*?[0-9]){1})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/; 
+  const regex =
+    /^(?=(?:.*?[A-Z]){1})(?=(?:.*?[0-9]){1})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/;
   return password.length > 8 && regex.test(password);
-};
+}
 
-function phoneNumberIsValid(phoneNumber: string): boolean{
+function phoneNumberIsValid(phoneNumber: string): boolean {
   // https://andersonarruda.com.br/article/ValidandocelularbrasileirocomExpressaoRegular/9
-  const regex = /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/;
+  return true;
+  const regex =
+    /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/;
   return regex.test(phoneNumber);
-};
+}
 
 //TODO
-function fieldOfActivityIsValid(fieldOfActivity: string): boolean{
-  
- return true;
-};
+function fieldOfActivityIsValid(fieldOfActivity: string): boolean {
+  return true;
+}
 
 // function cpfIsValid(cpf: String): boolean{
 //   // https://gist.github.com/joaohcrangel/8bd48bcc40b9db63bef7201143303937
@@ -109,7 +132,7 @@ function fieldOfActivityIsValid(fieldOfActivity: string): boolean{
 //       cpf == "66666666666" ||
 //       cpf == "77777777777" ||
 //       cpf == "88888888888" ||
-//       cpf == "99999999999" 
+//       cpf == "99999999999"
 //   ) {
 //       return false
 //   }
